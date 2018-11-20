5777157a676306d22a8b2cf565502f77387b41ce
kavyau <kavyaumapathi28@gmail.com>
2018-11-20 21:29:59 +0530

    added new variable

--- basics/src/Array.java
+++ basics/src/Array.java
@@ -2,5 +2,7 @@ public class Array {
 
     public static void main(String[] args) {
         System.out.println("Def. no Array elements");
+        int y = 10;
+        System.out.println("Number is : "+y);
     }
 }
