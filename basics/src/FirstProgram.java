b8c9737c076c3bceeb12c0dffbfaf7a115290de1
kavyau <kavyaumapathi28@gmail.com>
2018-11-20 20:48:04 +0530

    my first commit

---
+++ basics/src/FirstProgram.java
@@ -0,0 +1,26 @@
+public class FirstProgram {
+
+    public static void main(String[] args)
+    {
+        int array[]={1,2,3,4,5};
+        for(int x:array)
+        {
+        System.out.println(x);
+
+        }
+    }
+/*    Arrays
+1.  Minimum  element in a array
+2.  Max element in a array
+3.  Reverse a array
+4.  Sum all the elements in a array
+
+            Linkedlist
+1. Add a new element
+2. Delete a element
+3.  Rotate a list
+4. Given 2 lists, find duplicates in list
+5. Find 2 numbers in the list which sum up to max number
+6. reverse a list*/
+
+}
