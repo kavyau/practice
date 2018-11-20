2d33ea484722b8c0767ba40b919508976562a88e
kavyau <kavyaumapathi28@gmail.com>
2018-11-20 21:23:18 +0530

    Changed print statement in Array.java

--- basics/src/Array.java
+++ basics/src/Array.java
@@ -1,6 +1,6 @@
 public class Array {
 
     public static void main(String[] args) {
-        System.out.println("No Array elements");
+        System.out.println("Def. no Array elements");
     }
 }
