fe0ff62285df2005230c7ef6b841e2c3d2d2c372
kavyau <kavyaumapathi28@gmail.com>
2018-11-20 20:56:09 +0530

    added 2 more numbers

--- basics/src/FirstProgram.java
+++ basics/src/FirstProgram.java
@@ -2,7 +2,7 @@ public class FirstProgram {
 
     public static void main(String[] args)
     {
-        int array[]={1,2,3,4,5};
+        int array[]={1,2,3,4,5,6,7};
         for(int x:array)
         {
         System.out.println(x);
