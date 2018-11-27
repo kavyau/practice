cb9d2e535bbdc61814af0c1029d969dc2f67ce55
kavyau <kavyaumapathi28@gmail.com>
2018-11-27 21:16:55 +0530

    String reverse

--- strings/src/Reversal_string.java
+++ strings/src/Reversal_string.java
@@ -2,7 +2,7 @@ public class Reversal_string
 {
     public static void main(String[] args)
     {
-        String s="I love Australia";
+        String s="Australia";
         String temp="";
 
         for(int i=s.length()-1;i>=0;i--)
