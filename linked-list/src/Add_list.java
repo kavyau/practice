9ef91f1dcaeb754c957c00a6304886f5b182efd2
kavyau <kavyaumapathi28@gmail.com>
2018-12-11 21:01:46 +0530

    Creating and adding elements to list

---
+++ linked-list/src/Add_list.java
@@ -0,0 +1,21 @@
+import java.util.LinkedList;
+
+public class Add_list
+{
+    public static void main(String[] args)
+    {
+        //created linked list
+        LinkedList list=new LinkedList();
+        // adding elements to list
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(50);
+
+        System.out.println(list);
+        //list.remove();
+        //System.out.println(list);
+    }
+}
+//using collections
